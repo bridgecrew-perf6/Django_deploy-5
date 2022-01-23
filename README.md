cc89bbf6cc14033efc89ef2fb77fb9b87b10580d
Harold <telemac.dev@gmail.com>
2022-01-23 16:58:31 -0400

    Configurar arquivos para Travis-CI

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Django_deploy
+
+https://app.travis-ci.com/telemac-dev/Django_deploy.svg?branch=main
--- contato/views.py
+++ contato/views.py
@@ -4,4 +4,3 @@ from django.http import HttpResponse
 
 def index(requests):
     return HttpResponse('<h1>Óla Mundo</h1>')
-
