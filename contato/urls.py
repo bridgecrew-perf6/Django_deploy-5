a85844a68257464e7d581c5ed88bc858aad53d81
Harold <telemac.dev@gmail.com>
2022-01-24 22:22:46 -0400

    Configuração staticfile e mediafiles

--- contato/urls.py
+++ contato/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, hello 
+from .views import HomeView, ListaContatoView, hello
 
 urlpatterns = [
     path('hello', hello, name='hello-world'),
     path('', HomeView.as_view(), name='home'),
+    path('contato', ListaContatoView.as_view(), name='pessoa.index'),
 ]
--- contato/views.py
+++ contato/views.py
@@ -1,12 +1,18 @@
-from re import template
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Contato
 
 
 class HomeView(TemplateView):
     template_name = 'contato/index.html'
 
 
+class ListaContatoView(ListView):
+    model = Contato
+    queryset = Contato.objects.all().order_by('nome_completo')
+
+
 def hello(requests):
     return HttpResponse('<h1>Óla Mundo</h1><br><h2>Bonjour Monde</h2>')
+
